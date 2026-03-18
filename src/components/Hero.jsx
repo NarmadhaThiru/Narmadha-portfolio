531695834b01c400fb8f2703b6c2251e83eae71c
Narmadha Thirugnanam <narmadha.uthra@gmail.com>
2026-03-17 22:58:37 -0400

    adding info

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,8 +6,9 @@ export default function Hero() {
       id="hero"
       className="h-screen flex flex-col justify-center items-center text-center bg-gray-900 text-white"
     >
-      <div class="text-center" style="opacity: 1; transform: none;"><h1 class="text-6xl font-bold mb-4">Hi, I'm Narmadha Thirugnanam 👋</h1>
-        <p class="text-xl text-gray-300">Front-End Developer | React | TypeScript</p>
+      <div className="text-center">
+        <h1 className="text-6xl font-bold mb-4">Hi, I'm Narmadha Thirugnanam 👋</h1>
+        <p className="text-xl text-gray-300">Front-End Developer | React | TypeScript</p>
       </div>
     </section>
   );
